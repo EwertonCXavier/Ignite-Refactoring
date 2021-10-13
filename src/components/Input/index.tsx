b3a1b7da0cbee04c1d5091797c56de85b7ba5850
EwertonCXavier <ewertoncassianox@gmail.com>
2021-10-13 08:40:43 -0400

    Finalizacao das refatoracoes em React e Typescript

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,14 +3,24 @@ import {
   useRef,
   useState,
   useCallback,
+  ReactNode,
+  InputHTMLAttributes
 } from 'react';
 
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface Props {
+  name: string;
+}
+
+type InputProps = InputHTMLAttributes<HTMLInputElement> & Props;
+
+
+
+export function Input ({ name, ...rest }: InputProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -23,7 +33,6 @@ const Input = ({ name, icon: Icon, ...rest }) => {
 
   const handleInputBlur = useCallback(() => {
     setIsFocused(false);
-
     setIsFilled(!!inputRef.current?.value);
   }, []);
 
@@ -37,7 +46,6 @@ const Input = ({ name, icon: Icon, ...rest }) => {
 
   return (
     <Container isFilled={isFilled} isFocused={isFocused}>
-      {Icon && <Icon size={20} />}
 
       <input
         onFocus={handleInputFocus}
@@ -49,5 +57,3 @@ const Input = ({ name, icon: Icon, ...rest }) => {
     </Container>
   );
 };
-
-export default Input;
